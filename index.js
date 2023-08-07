89de05fb9fca46832625587c79a9bfe470dbbf76
wenhan-lui <133980867+wenhan-lui@users.noreply.github.com>
2023-08-07 08:45:49 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
